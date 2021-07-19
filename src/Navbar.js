1db4dfad26967886dd28fb6deb05c600edaedef7
avnish ranwa <avnishranwa@jklu.edu.in>
2021-07-19 23:56:41 +0530

    navbar

---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './navbar.css'
+import Brand from './brand.svg'
+import Logo from './logo.png'
+import {Link} from "react-router-dom"
+//Navbar
+const Navbar = ()=>{
+    return(
+        <div className="nav">
+            <div className="brand">
+                {/* Brand */}
+                <img src={Brand} alt="brand-logo"/>
+            </div>
+            <div className="logo">
+                {/* Logo */}
+                <img src={Logo} alt="logo"/>
+            </div>
+           <button className="btn">Sign In</button>
+        </div>
+    )
+}
+
+export default Navbar;
